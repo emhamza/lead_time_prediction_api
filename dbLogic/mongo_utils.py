d6d160a4a00762de403bfa90c75d9ed444820aab
emhamza <imumairhamza@gmail.com>
2025-09-09 19:04:08 +0500

    Write the funciton to load the predicitons

--- dbLogic/mongo_utils.py
+++ dbLogic/mongo_utils.py
@@ -34,3 +34,30 @@ def save_prediction_to_mongo(
         print(f"🔄 Replaced existing prediction for vendor {vendor_id} in '{collection_name}'")
     else:
         print(f"🆕 Inserted new prediction for vendor {vendor_id} into '{collection_name}'")
+
+def load_predictions_from_mongo(
+        collection_name=PRED_COLLECTION_NAME
+):
+    collection = get_collection(collection_name)
+    documents = list(collection.find({}))
+
+    records = []
+    for doc in documents:
+        vendor_id = doc.get("vendor_id")
+        saved_at = doc.get("saved_at")
+        prediction_list = doc.get("prediction", {}).get("predictions", [])
+        for pred in prediction_list:
+            record = {
+                "vendor_id": vendor_id,
+                "saved_at": saved_at,
+                "PO_ID": pred.get("PO_ID"),
+                "p50_survival_time": pred.get("p50_survival_time"),
+                "p90_survival_time": pred.get("p90_survival_time"),
+                "risk_score": pred.get("risk_score"),
+                "model_version": pred.get("model_version"),
+                "model_id": pred.get("model_id"),
+                "survival_curve": pred.get("survival_curve")
+            }
+            records.append(record)
+    df = pd.DataFrame(records)
+    return df
