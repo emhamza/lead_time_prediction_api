83201c0fd53566dfef133076dee825a8b15bfcd9
M. Ahmad Uzair <mauzair19@gmail.com>
2025-09-09 11:51:58 +0500

    rebasing

--- src/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List, Dict, Any, Optional
-class TokenResponse(BaseModel):
-    access_token: str
-    token_type: str
-
-class LoginRequest(BaseModel):
-    username: str
-    password: str
